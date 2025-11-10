d1d69951d6f2a075f17e77e37540d742bbdb4c59
vinaynalla109 <vinaynalla109@gmail.com>
2025-11-10 10:06:50 +0530

    My Second Commit Added New Class

--- src/com/github/Demo.java
+++ src/com/github/Demo.java
@@ -3,7 +3,7 @@ package com.github;
 public class Demo {
 	
 	void print() {
-       System.out.println("Welcome to github session");
+       System.out.println("Welcome to github session Tata By By");
        
 	}
 }
---
+++ src/com/github/Demo1.java
@@ -0,0 +1,8 @@
+package com.github;
+
+public class Demo1 {
+	void display() {
+		System.out.println("Het THis is My Second Class");
+	}
+
+}
